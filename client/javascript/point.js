0434649b8d9f3466b30a4a4e0bb22489726a9f19
Mugna00 <46057469+Mugna00@users.noreply.github.com>
2022-07-08 14:32:53 +0200

    Bug fix 3

--- client/javascript/point.js
+++ client/javascript/point.js
@@ -2,7 +2,7 @@ export class Point {
     constructor() {
         this.turno = 0;
         this.src = []
-        this.src.push(new img("f1.jpg",0,0,200,200));
+        this.src.push(new img("f1.jpeg",0,0,200,200));
         // this.src = [["f1.jpeg", "face"], ["f2.jpg", "face"], ["f3.jpg", "Chair"], ["f4.jpg", "face"], ["f5.jpeg", "face"], ["f6.jpg", "face"], ["f7.jpg", "face"], ["f8.jpg", "face"]];
         this.length = this.src.length
     }
