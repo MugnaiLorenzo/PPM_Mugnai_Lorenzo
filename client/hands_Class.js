d69df176a44800f4981003828aebcad425ef967f
Mugna00 <46057469+Mugna00@users.noreply.github.com>
2022-07-08 15:58:02 +0200

    Bug fix 3

--- client/hands_Class.js
+++ client/hands_Class.js
@@ -64,8 +64,10 @@ export class Hands_Class {
         this.point = img;
         this.img = new Image();
         this.img.src = "./image/opere/" + this.point.src;
-        this.img.onloadeddata = this.canvasCtx.drawImage(this.img, 0, 0, this.canvasElement.width, this.canvasElement.height);
         this.canvasCtx.save();
+        this.canvasCtx.beginPath();
+        this.canvasCtx.clearRect(0, 0, this.canvasElement.width * -1, this.canvasElement.height);
+        this.img.onloadeddata = this.canvasCtx.drawImage(this.img, 0, 0, this.canvasElement.width, this.canvasElement.height);
         this.hands.onResults(this.onResults.bind(this));
     }
 
