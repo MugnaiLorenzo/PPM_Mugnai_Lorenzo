05d3f9668266f4315a0ea5aca92517389c3a408d
Mugna00 <46057469+Mugna00@users.noreply.github.com>
2022-07-08 16:43:12 +0200

    Bug fix 3

--- client/hands_Class.js
+++ client/hands_Class.js
@@ -13,6 +13,7 @@ export class Hands_Class {
         this.y_c = null;
         this.w = null;
         this.h = null;
+        this.ready = false;
         this.hands = new Hands({
             locateFile: (file) => {
                 return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
@@ -38,31 +39,35 @@ export class Hands_Class {
     }
 
     onResults(results) {
-        this.canvasCtx.save();
-        this.canvasCtx.beginPath();
-        this.canvasCtx.clearRect(0, 0, this.canvasElement.width * -1, this.canvasElement.height);
-        this.canvasCtx.drawImage(this.img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+        if(this.ready === true){
+            this.canvasCtx.save();
+            this.canvasCtx.beginPath();
+            this.canvasCtx.clearRect(0, 0, this.canvasElement.width * -1, this.canvasElement.height);
+            this.canvasCtx.drawImage(this.img, 0, 0, this.canvasElement.width, this.canvasElement.height);
 
-        // results.image, 0, 0, canvasElement.width, canvasElement.height);
-        this.canvasCtx.strokeRect(this.x_c, this.y_c, this.w, this.h);
-        if (results.multiHandLandmarks) {
-            for (const landmarks of results.multiHandLandmarks) {
-                let x = landmarks[8].x * this.canvasElement.width;
-                let y = landmarks[8].y * this.canvasElement.height;
-                this.canvasCtx.arc(x, y, 5, 0, 2 * Math.PI);
-                this.canvasCtx.fill();
-                if (x > this.x_c && x < (this.x_c + this.w) && y > this.y_c && y < (this.y_c + this.h)) {
-                    if (this.num_solution === 0) {
-                        this.num_solution++;
-                        this.sock.emit('point');
+            // results.image, 0, 0, canvasElement.width, canvasElement.height);
+            this.canvasCtx.strokeRect(this.x_c, this.y_c, this.w, this.h);
+            if (results.multiHandLandmarks) {
+                for (const landmarks of results.multiHandLandmarks) {
+                    let x = landmarks[8].x * this.canvasElement.width;
+                    let y = landmarks[8].y * this.canvasElement.height;
+                    this.canvasCtx.arc(x, y, 5, 0, 2 * Math.PI);
+                    this.canvasCtx.fill();
+                    if (x > this.x_c && x < (this.x_c + this.w) && y > this.y_c && y < (this.y_c + this.h)) {
+                        if (this.num_solution === 0) {
+                            this.num_solution++;
+                            this.sock.emit('point');
+                            this.ready = false;
+                        }
                     }
                 }
             }
+            this.canvasCtx.restore()
         }
-        this.canvasCtx.restore()
     }
 
     start(img) {
+        this.ready = false;
         this.num_solution = 0;
         this.point = img;
         this.img = new Image();
@@ -80,6 +85,7 @@ export class Hands_Class {
         this.y_c = this.point.y;
         this.w = this.point.width * this.canvasElement.width / 950;
         this.h = this.point.height * this.canvasElement.width / 950;
+        this.ready = true;
     }
 
 }
