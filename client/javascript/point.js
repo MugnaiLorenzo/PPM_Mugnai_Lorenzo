0f0469d5707861fe1e9023af0f89c6d866a7d5cb
Mugna00 <46057469+Mugna00@users.noreply.github.com>
2022-07-09 16:53:40 +0200

    Bug fix 3

--- client/javascript/point.js
+++ client/javascript/point.js
@@ -4,11 +4,11 @@ export class Point {
         this.src = []
         this.src.push(new img("f1.jpg", 1000, 802, 481, 27, 235, 544));
         this.src.push(new img("f2.jpg", 1587, 1057, 1195, 238, 343, 235));
-        this.src.push(new img("f3.jpg", 945, 1000, 0, 0, 200, 200));
-        this.src.push(new img("f4.jpg", 1000, 842, 0, 0, 200, 200));
-        this.src.push(new img("f5.jpg", 1500, 1147, 0, 0, 200, 200));
-        this.src.push(new img("f6.jpg", 1094, 749, 0, 0, 200, 200));
-        this.src.push(new img("f6.jpg", 1209, 556, 0, 0, 200, 200));
+        this.src.push(new img("f3.jpg", 945, 1000, 285, 507, 270, 294));
+        this.src.push(new img("f4.jpg", 1000, 842, 580, 444, 72, 72));
+        this.src.push(new img("f5.jpg", 1500, 1147, 0, 728, 69, 404));
+        this.src.push(new img("f6.jpg", 1094, 749, 150, 238, 827, 200));
+        this.src.push(new img("f7.jpg", 1209, 556, 325, 200, 134, 369));
         this.length = this.src.length
     }
 
