eec88cf792c6b117aefb481e1afd1c7ae803d9f5
Mugna00 <46057469+Mugna00@users.noreply.github.com>
2022-07-09 15:38:23 +0200

    Bug fix 3

--- client/javascript/point.js
+++ client/javascript/point.js
@@ -2,11 +2,11 @@ export class Point {
     constructor() {
         this.turno = 0;
         this.src = []
-        this.src.push(new img("f1.jpeg", 900, 450, 0, 0, 200, 200));
-        this.src.push(new img("f2.jpg", 900, 450, 0, 0, 200, 200));
-        this.src.push(new img("f3.jpg", 900, 450, 0, 0, 200, 200));
-        this.src.push(new img("f4.jpg", 900, 450, 0, 0, 200, 200));
-        this.src.push(new img("f5.jpeg", 900, 450, 0, 0, 200, 200));
+        this.src.push(new img("f1.jpg", 1000, 802, 0, 0, 200, 200));
+        this.src.push(new img("f2.jpg", 1587, 1057, 0, 0, 200, 200));
+        this.src.push(new img("f3.jpg", 945, 1000, 0, 0, 200, 200));
+        this.src.push(new img("f4.jpg", 1000, 842, 0, 0, 200, 200));
+        this.src.push(new img("f5.jpg", 1500, 1147, 0, 0, 200, 200));
         this.src.push(new img("f6.jpg", 900, 450, 0, 0, 200, 200));
         this.src.push(new img("f7.jpg", 900, 450, 0, 0, 200, 200));
         this.src.push(new img("f8.jpg", 900, 450, 0, 0, 200, 200));
