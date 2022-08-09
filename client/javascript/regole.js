7eee0907ce1f82176f45eaebe631a71f85bf1565
loren <lorenzomugnai43@gmail.com>
2022-08-09 11:49:48 +0200

    Bug fix 5

--- client/javascript/regole.js
+++ client/javascript/regole.js
@@ -1,14 +1,2 @@
-const fs = require('fs');
-
-// read JSON object from file
-fs.readFile('../data.json', 'utf-8', (err, data) => {
-    if (err) {
-        throw err;
-    }
-
-    // parse JSON object
-    const user = JSON.parse(data.toString());
-
-    // print JSON object
-    console.log(user);
-});
+import data from '../data.json';
+console.log(data);
